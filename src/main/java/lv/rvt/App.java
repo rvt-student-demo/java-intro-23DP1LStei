93eda4c094629013f265b537e32d33f1a4b9b450
23DP1LStei <166118292+23DP1LStei@users.noreply.github.com>
2024-09-10 10:34:33 +0000

    uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,32 +1,24 @@
 package lv.rvt;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        long lastId = -1;
-        try (BufferedReader reader = Utils.getReader("data.csv")) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What's your name?: ");
+        String name = scanner.nextLine();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
-        try (BufferedWriter writter = Utils.getWriter("data.csv")) {
-            // writter.newLine();
-            // writter.write("2, user, 123");
-            // writter.flush();
-        
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
+        System.out.println("What's your last name?: ");
+        String lastname = scanner.nextLine();
 
+        System.out.println("What's your group?: ");
+        String group = scanner.nextLine();
+
+
+        System.out.println("Student: " + name + " " + lastname + ", group: " + group);
     }
 
 }
