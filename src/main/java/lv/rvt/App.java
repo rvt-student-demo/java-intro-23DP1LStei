9549f21ac9e58638fdf41c3d008f355833c2d90a
23DP1LStei <166118292+23DP1LStei@users.noreply.github.com>
2024-10-08 07:42:03 +0000

    Advanced astrology

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -28,8 +28,40 @@ public class App {
         for (int i = 0; i < size; i++) {
             printStars(i+1);
         }
+        
     }
     
+
+    public static void printSpaces(int number){
+        for (int i = 0; i < number; i++) {
+            System.out.print(" ");
+        }
+        
+
+    }
+
+    public static void printTriangle2(int size) {
+        System.out.println();
+        for (int i = 0; i < size; i++) {
+            printSpaces(size-i-1);
+            printStars(i+1);
+        }
+    }
+
+
+
+    public static void christmasTree(int height) {
+        System.out.println();
+        for (int i = 0; i < height; i++) {
+            printSpaces(height-i-1);
+            printStars(1+i*2);
+        }
+        for (int i = 0; i < 2; i++) {
+            printSpaces(height-2);
+            printStars(3);
+            
+        }
+    }
     public static void main(String[] args) {
         printStars(5);
         printStars(3);
@@ -40,5 +72,10 @@ public class App {
         printRectangle(10, 4);
 
         printTriangle(5);
+
+        printTriangle2(5);
+
+
+        christmasTree(10);
     }
 }
