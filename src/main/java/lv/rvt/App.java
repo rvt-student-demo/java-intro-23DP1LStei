08ce6b671f23e1b618b3ee3b3cb8e8fa27271a7d
23DP1LStei <166118292+23DP1LStei@users.noreply.github.com>
2024-10-08 06:18:38 +0000

    Star sign

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,44 +1,44 @@
 package lv.rvt;
 
-import java.util.Scanner;
-
 public class App {
-    public static void main( String[] args )
-    {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Give numbers: ");
-        int summa = 0;
-        int skaitlis = 0;
-        int sksk = 0;
-        int even = 0;
-        int odd = 0;
-
-        while (true) { 
-            skaitlis = Integer.valueOf(scanner.nextLine());
-            if (skaitlis != -1) {
-                summa = summa + skaitlis;
-                sksk = sksk +1;
-                if (skaitlis%2 == 0) {
-                    even = even + 1;
-                } else {
-                    odd = odd + 1;
-                }
-            } else {
-                break;
-            }
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
         }
-        System.out.println("Thx! Bye!");
-        System.out.println("Sum: " + summa);
-        System.out.println("Numbers: " + sksk);
-        double average = (double)summa/sksk;
-        System.out.println("Average: " + average);
-        System.out.println("Even: " + even);
-        System.out.println("Odd: " + odd);
+        System.out.println("");
+    }
 
-        
+    public static void printSquare(int size) {
+        System.out.println("");
+        for (int i = 0; i < size; i++) {
+            System.out.println("***");
+        }
+    }
 
+
+    public static void printRectangle(int width, int height) {
+        System.out.println("");
+        for (int i = 0; i < height; i++) {
+            printStars(width);
+        }
+    }
+
+    public static void printTriangle(int size) {
+        System.out.println("");
+        for (int i = 0; i < size; i++) {
+            printStars(i+1);
+        }
     }
     
+    public static void main(String[] args) {
+        printStars(5);
+        printStars(3);
+        printStars(9);
+        
+        printSquare(5);
 
+        printRectangle(10, 4);
+
+        printTriangle(5);
+    }
 }
